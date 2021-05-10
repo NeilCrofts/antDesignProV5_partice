8789f02524d02ce7440eb790ffb6a0a93d32885c
NeilCrofts <neilcrofts@foxmail.com>
2021-05-10 16:01:15 +0800

    代码重构

--- src/pages/BasciList/index.tsx
+++ src/pages/BasciList/index.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Table, Row, Col, Card, Pagination, Space, Modal as AntdModal, message } from 'antd';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
-import { useRequest,useIntl } from 'umi';
-import {useSessionStorageState} from 'ahooks'
+import { useRequest, useIntl } from 'umi';
+import { useSessionStorageState } from 'ahooks';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import ActionBuilder from './builder/ActionBuilder';
 import ColumnBuilder from './builder/ColumnBuilder';
@@ -10,8 +10,7 @@ import Modal from './component/Modal';
 import style from './index.less';
 
 const Index = () => {
-  const [page, setPage] = useState(1); // 设置当前页数和函数setPage
-  const [per_page, setPerPage] = useState(12); // 设置每页的条目数和函数setPerPage
+  const [pageQuery, setPageQuery] = useState(''); // 设置第几页and每页多少项
   // 定义排序请求字符
   const [sortQuery, setSortQuery] = useState('');
   // 控制弹窗关闭
@@ -22,12 +21,15 @@ const Index = () => {
   const [selectedRows, setSelectedRows] = useState([]);
   const { confirm } = AntdModal;
   // 将列表存为全局
-  const [tableColumn, setTableColumn] = useSessionStorageState<BasicListApi.Filed[]>('basicListTableColumn',[]);
+  const [tableColumn, setTableColumn] = useSessionStorageState<BasicListApi.Filed[]>(
+    'basicListTableColumn',
+    [],
+  );
   const lang = useIntl();
 
   // useRequest 获取 列表(admit list) 数据
   const init = useRequest<{ data: BasicListApi.ListData }>(
-    `https://public-api-v2.aspirantzhang.com/api/admins?X-API-KEY=antd&page=${page}&per_page=${per_page}${sortQuery}`,
+    `https://public-api-v2.aspirantzhang.com/api/admins?X-API-KEY=antd${pageQuery}${sortQuery}`,
   );
 
   const request = useRequest(
@@ -52,7 +54,7 @@ const Index = () => {
       manual: true,
       onSuccess: (data) => {
         message.success({
-          content: data.message,
+          content: data?.message,
           key: 'process',
         });
       },
@@ -65,41 +67,34 @@ const Index = () => {
   // 当page||per_page变量改变后，运行init.run()，解决init.run会异步执行的问题
   useEffect(() => {
     init.run();
-  }, [page, per_page, sortQuery]);
+  }, [pageQuery, sortQuery]);
 
   useEffect(() => {
     if (init?.data?.layout?.tableColumn) {
-      setTableColumn(ColumnBuilder(init?.data?.layout?.tableColumn, actionHandler));
+      setTableColumn(ColumnBuilder(init.data.layout.tableColumn, actionHandler));
     }
   }, [init?.data?.layout?.tableColumn]);
+  // 弹窗有数据后才打开窗口
+  useEffect(()=>{
+    if(modalUri){
+      setModalVisible(true);
+    }
+  },[modalUri])
 
-  const batchOverView = (dataSourse:BasicListApi.Filed[]) => {
-    return (
-      <Table
-        size="small"
-        rowKey="id"
-        columns={tableColumn?[tableColumn[0] || {}, tableColumn[1] || {}]:[]}
-        dataSource={dataSourse}
-        pagination={false}
-      ></Table>
-    );
-  };
-
-  // 页面头部
-  const searchLayout = () => {};
-
-  function actionHandler(action: BasicListApi.Action, record: any) {
+  function actionHandler(action: BasicListApi.Action, record: BasicListApi.Filed) {
+    const operationName = lang.formatMessage({
+      id: `basic-list.list.actionHandler.operation.${action.action}`,
+    });
     switch (action.action) {
       // 点击add添加按钮 显示弹窗和设置请求地址
       case 'modal':
         setModalUri(
-          (action.uri||"").replace(/:\w+/g, (field) => {
+          (action.uri || '').replace(/:\w+/g, (field) => {
             // field 为正则查询到的 :id
             // 取出record 中的 id 属性的值
             return record[field.replace(':', '')];
           }),
         );
-        setModalVisible(true);
         break;
       case 'reload':
         init.run();
@@ -107,15 +102,15 @@ const Index = () => {
       case 'delete':
       case 'deletePermanently':
       case 'restore':
-        const operationName = lang.formatMessage({
-          id:`basic-list.list.actionHandler.operation.${action.action}`,
-        })
         confirm({
-          title:lang.formatMessage({
-            id:'basic-list.list.actionHandler.confirmTitle'
-          },{
-            operationName:operationName
-          }),
+          title: lang.formatMessage(
+            {
+              id: 'basic-list.list.actionHandler.confirmTitle',
+            },
+            {
+              operationName,
+            },
+          ),
           // title: `Do you Want to ${action.action} these items?`,
           icon: <ExclamationCircleOutlined />,
           content: batchOverView(Object.keys(record).length ? [record] : selectedRows),
@@ -130,14 +125,70 @@ const Index = () => {
               ids: Object.keys(record).length ? [record.id] : selectedRowKeys,
             });
           },
-          onCancel() {
-          },
+          onCancel() {},
         });
         break;
       default:
         break;
     }
   }
+
+  function batchOverView(dataSourse: BasicListApi.Filed[]) {
+    return (
+      <Table
+        size="small"
+        rowKey="id"
+        columns={tableColumn ? [tableColumn[0] || {}, tableColumn[1] || {}] : []}
+        dataSource={dataSourse}
+        pagination={false}
+      ></Table>
+    );
+  };
+
+  const pagenationChangeHandler = (page: any, per_page: any) => {
+    setPageQuery(`&page=${page}&per_page=${per_page}`);
+    // 异步 会先于前两行执行
+    // init.run()
+  };
+
+  // 列表排列功能
+  const tableChangeHandler = (_: any, __: any, sorter: any) => {
+    if (sorter.order === undefined) {
+      setSortQuery('');
+    } else {
+      const orderBy = sorter.order === 'ascend' ? 'asc' : 'desc';
+      setSortQuery(`&sort=${sorter.field}&order=${orderBy}`);
+    }
+  };
+
+  // 列表选择功能
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (_selectedRowKeys: any, _selectedRows: any) => {
+      setSelectedRowKeys(_selectedRowKeys);
+      setSelectedRows(_selectedRows);
+    },
+  };
+
+  const patchToolBar = () => {
+    return (
+      selectedRowKeys.length > 0 && (
+        <Space>{ActionBuilder(init?.data?.layout?.batchToolBar, actionHandler)}</Space>
+      )
+    );
+  };
+
+  const hideModal = (reload = false) => {
+    setModalVisible(false);
+    // 清空Uri地址
+    setModalUri('');
+    // 弹窗关闭后，刷新界面数据，默认不刷新
+    if (reload) init.run();
+  };
+
+  // 页面头部
+  const searchLayout = () => {};
+
   // 右上角按钮
   const beforeTableLayout = () => {
     return (
@@ -149,29 +200,7 @@ const Index = () => {
       </Row>
     );
   };
-  const pagenationChangeHandler = (_page: any, _per_page: any) => {
-    setPage(_page);
-    setPerPage(_per_page);
-    // 异步 会先于前两行执行
-    // init.run()
-  };
-  // 列表排列功能
-  const tableChangeHandler = (_: any, __: any, sorter: any) => {
-    if (sorter.order === undefined) {
-      setSortQuery('');
-    } else {
-      const orderBy = sorter.order === 'ascend' ? 'asc' : 'desc';
-      setSortQuery(`&sort=${sorter.field}&order=${orderBy}`);
-    }
-  };
-  // 列表选择功能
-  const rowSelection = {
-    selectedRowKeys,
-    onChange: (_selectedRowKeys: any, _selectedRows: any) => {
-      setSelectedRowKeys(_selectedRowKeys);
-      setSelectedRows(_selectedRows);
-    },
-  };
+
   // 页面尾部
   const afterTableLayout = () => {
     return (
@@ -194,19 +223,6 @@ const Index = () => {
     );
   };
 
-  const hideModal = (reload = false) => {
-    setModalVisible(false);
-    // 弹窗关闭后，刷新界面数据，默认不刷新
-    if (reload) init.run();
-  };
-
-  const patchToolBar = () => {
-    return (
-      selectedRowKeys.length > 0 && (
-        <Space>{ActionBuilder(init?.data?.layout?.batchToolBar, actionHandler)}</Space>
-      )
-    );
-  };
   return (
     <PageContainer>
       {searchLayout()}
@@ -216,6 +232,7 @@ const Index = () => {
           rowKey="id"
           dataSource={init?.data?.dataSource}
           columns={tableColumn}
+          loading = {init?.loading}
           pagination={false}
           onChange={tableChangeHandler}
           rowSelection={rowSelection}
